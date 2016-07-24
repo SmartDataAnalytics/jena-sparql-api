83789d2ea9bbdc241d7982a197ad81da1a465529
Claus Stadler <RavenArkadon@googlemail.com>
2016-07-24 14:50:32 +0200

    wip

---
+++ jena-sparql-api-cache/src/main/java/org/aksw/isomorphism/LinearCandidateLists.java
@@ -0,0 +1,17 @@
+package org.aksw.isomorphism;
+
+import org.aksw.commons.collections.multimaps.BiHashMultimap;
+
+/**
+ * Given (1) a multimap of candidate mappings a and b and (2) two lists of x and y
+ * find all possbile sequences of x mapping to ys 
+ * 
+ * - test if the src and tgt set of the mapping are consecutive according to xy and y.
+ * 
+ * @author raven
+ *
+ */
+public class LinearCandidateLists {
+    protected BiHashMultimap<A, B> remaining;
+
+}
