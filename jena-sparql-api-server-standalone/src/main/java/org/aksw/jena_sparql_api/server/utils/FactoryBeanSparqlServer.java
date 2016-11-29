0c040e3fe677d09358d539ff079ddecadfdc029a
Claus Stadler <RavenArkadon@googlemail.com>
2016-11-29 15:08:53 +0100

    factory bean for sparql servers

---
+++ jena-sparql-api-server-standalone/src/main/java/org/aksw/jena_sparql_api/server/utils/FactoryBeanSparqlServer.java
@@ -0,0 +1,91 @@
+package org.aksw.jena_sparql_api.server.utils;
+
+import java.util.function.Function;
+
+import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
+import org.aksw.jena_sparql_api.core.SparqlService;
+import org.aksw.jena_sparql_api.core.SparqlServiceFactory;
+import org.aksw.jena_sparql_api.core.SparqlServiceImpl;
+import org.aksw.jena_sparql_api.stmt.SparqlStmt;
+import org.aksw.jena_sparql_api.stmt.SparqlStmtParserImpl;
+import org.aksw.jena_sparql_api.web.server.ServerUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.jena.query.Syntax;
+import org.apache.jena.sparql.core.DatasetDescription;
+import org.eclipse.jetty.server.Server;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+
+public class FactoryBeanSparqlServer {
+    protected Integer port;
+    protected SparqlServiceFactory sparqlServiceFactory;
+    protected Function<String, SparqlStmt> sparqlStmtParser;
+
+    public int getPort() {
+        return port;
+    }
+
+    public FactoryBeanSparqlServer setPort(int port) {
+        this.port = port;
+
+        return this;
+    }
+
+    public SparqlServiceFactory getSparqlServiceFactory() {
+        return sparqlServiceFactory;
+    }
+
+    public FactoryBeanSparqlServer setSparqlServiceFactory(SparqlServiceFactory sparqlServiceFactory) {
+        this.sparqlServiceFactory = sparqlServiceFactory;
+
+        return this;
+    }
+
+    public FactoryBeanSparqlServer setSparqlServiceFactory(QueryExecutionFactory qef) {
+        SparqlServiceFactory ssf = new SparqlServiceFactory() {
+            @Override
+            public SparqlService createSparqlService(String serviecUri, DatasetDescription datasetDescription, HttpClient httpClient) {
+                 return new SparqlServiceImpl(qef, null);
+            }
+        };
+
+        this.setSparqlServiceFactory(ssf);
+
+        return this;
+    }
+
+    public Function<String, SparqlStmt> getSparqlStmtParser() {
+        return sparqlStmtParser;
+    }
+
+    public FactoryBeanSparqlServer setSparqlStmtParser(Function<String, SparqlStmt> sparqlStmtParser) {
+        this.sparqlStmtParser = sparqlStmtParser;
+
+        return this;
+    }
+
+
+    public Server create() {
+        if(port == null) {
+            port = 7531;
+        }
+
+        if(sparqlStmtParser == null) {
+            sparqlStmtParser = SparqlStmtParserImpl.create(Syntax.syntaxARQ, true);
+        }
+
+        if(sparqlServiceFactory == null) {
+            throw new RuntimeException("SparqlServiceFactory must not be null");
+        }
+
+        GenericWebApplicationContext rootContext = new GenericWebApplicationContext();
+        rootContext.getBeanFactory().registerSingleton("sparqlServiceFactory", sparqlServiceFactory);
+        rootContext.getBeanFactory().registerSingleton("sparqlStmtParser", sparqlStmtParser);
+
+        Server result = ServerUtils.startServer(port, new WebAppInitializerSparqlService(rootContext));
+        return result;
+    }
+
+    public static FactoryBeanSparqlServer newInstance() {
+        return new FactoryBeanSparqlServer();
+    }
+}
--- jena-sparql-api-server-standalone/src/main/java/org/aksw/jena_sparql_api/server/utils/SparqlServerUtils.java
+++ jena-sparql-api-server-standalone/src/main/java/org/aksw/jena_sparql_api/server/utils/SparqlServerUtils.java
@@ -2,21 +2,33 @@ package org.aksw.jena_sparql_api.server.utils;
 
 import org.aksw.jena_sparql_api.core.SparqlServiceFactory;
 import org.aksw.jena_sparql_api.stmt.SparqlStmtParser;
-import org.aksw.jena_sparql_api.web.server.ServerUtils;
 import org.eclipse.jetty.server.Server;
-import org.springframework.web.context.support.GenericWebApplicationContext;
 
 public class SparqlServerUtils {
 
+    /**
+     * Use FactoryBeanSparqlServer instead
+     *
+     * @param ssf
+     * @param sparqlStmtParser
+     * @param port
+     * @return
+     */
+    @Deprecated
     public static Server startSparqlEndpoint(SparqlServiceFactory ssf, SparqlStmtParser sparqlStmtParser, int port) {
+        Server result = FactoryBeanSparqlServer.newInstance()
+                .setPort(port)
+                .setSparqlStmtParser(sparqlStmtParser)
+                .setSparqlServiceFactory(ssf).create();
+        return result;
+
 //      AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
 //      rootContext.refresh();
-      GenericWebApplicationContext rootContext = new GenericWebApplicationContext();
-      rootContext.getBeanFactory().registerSingleton("sparqlServiceFactory", ssf);
-      rootContext.getBeanFactory().registerSingleton("sparqlStmtParser", sparqlStmtParser);
-
-      Server result = ServerUtils.startServer(port, new WebAppInitializerSparqlService(rootContext));
-      return result;
+//      GenericWebApplicationContext rootContext = new GenericWebApplicationContext();
+//      rootContext.getBeanFactory().registerSingleton("sparqlServiceFactory", ssf);
+//      rootContext.getBeanFactory().registerSingleton("sparqlStmtParser", sparqlStmtParser);
+//
+//      Server result = ServerUtils.startServer(port, new WebAppInitializerSparqlService(rootContext));
   }
 
 }
