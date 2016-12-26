2627c574f5c7b63e368979523c62a1fea40361f8
Claus Stadler <RavenArkadon@googlemail.com>
2016-12-26 20:13:05 +0100

    small refactoring

--- benchmarking/sparqlqc-jena3/sparqlqc-core/src/main/java/fr/inrialpes/tyrexmo/testqc/MainTestContain.java
+++ benchmarking/sparqlqc-jena3/sparqlqc-core/src/main/java/fr/inrialpes/tyrexmo/testqc/MainTestContain.java
@@ -52,65 +52,6 @@ import com.google.common.base.Predicate;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 
-class TestCase {
-    protected Query source;
-    protected Query target;
-    protected boolean expectedResult;
-
-    public TestCase(Query source, Query target, boolean expectedResult) {
-        super();
-        this.source = source;
-        this.target = target;
-        this.expectedResult = expectedResult;
-    }
-
-    public Query getSource() {
-        return source;
-    }
-
-    public Query getTarget() {
-        return target;
-    }
-
-    public boolean getExpectedResult() {
-        return expectedResult;
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + (expectedResult ? 1231 : 1237);
-        result = prime * result + ((source == null) ? 0 : source.hashCode());
-        result = prime * result + ((target == null) ? 0 : target.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        TestCase other = (TestCase) obj;
-        if (expectedResult != other.expectedResult)
-            return false;
-        if (source == null) {
-            if (other.source != null)
-                return false;
-        } else if (!source.equals(other.source))
-            return false;
-        if (target == null) {
-            if (other.target != null)
-                return false;
-        } else if (!target.equals(other.target))
-            return false;
-        return true;
-    }
-}
-
 public class MainTestContain {
     private static final Logger logger = LoggerFactory.getLogger(MainTestContain.class);
 
