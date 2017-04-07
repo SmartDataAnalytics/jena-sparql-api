964738eba0344767cd9d20e4bade15d961aaf3e9
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-04-07 16:44:10 +0200

    optimized projections in the cache

--- jena-sparql-api-cache/src/main/java/org/aksw/jena_sparql_api/concept_cache/core/QueryExecutionViewMatcherMaster.java
+++ jena-sparql-api-cache/src/main/java/org/aksw/jena_sparql_api/concept_cache/core/QueryExecutionViewMatcherMaster.java
@@ -1,5 +1,6 @@
 package org.aksw.jena_sparql_api.concept_cache.core;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.IdentityHashMap;
 import java.util.List;
@@ -34,6 +35,7 @@ import org.apache.jena.sparql.algebra.Algebra;
 import org.apache.jena.sparql.algebra.Op;
 import org.apache.jena.sparql.algebra.OpAsQuery;
 import org.apache.jena.sparql.algebra.op.OpNull;
+import org.apache.jena.sparql.algebra.op.OpProject;
 import org.apache.jena.sparql.algebra.op.OpService;
 import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.binding.Binding;
@@ -46,18 +48,18 @@ import com.google.common.cache.Cache;
 import com.google.common.collect.Range;
 
 public class QueryExecutionViewMatcherMaster
-	extends QueryExecutionBaseSelect
+    extends QueryExecutionBaseSelect
 {
-	private static final Logger logger = LoggerFactory.getLogger(QueryExecutionViewMatcherMaster.class);
+    private static final Logger logger = LoggerFactory.getLogger(QueryExecutionViewMatcherMaster.class);
 
-	protected OpRewriteViewMatcherStateful opRewriter;
-	protected ExecutorService executorService;
+    protected OpRewriteViewMatcherStateful opRewriter;
+    protected ExecutorService executorService;
 
-	// The jena context - used for setting up cache entries
-	// TODO Not sure if this was better part of the rewriter - or even a rewriteContext object
-	protected Context context;
+    // The jena context - used for setting up cache entries
+    // TODO Not sure if this was better part of the rewriter - or even a rewriteContext object
+    protected Context context;
 
-	// TODO Maybe add a decider which determines whether the result set of a query should be cached
+    // TODO Maybe add a decider which determines whether the result set of a query should be cached
 
     protected long indexResultSetSizeThreshold;
 
@@ -68,29 +70,29 @@ public class QueryExecutionViewMatcherMaster
     protected Integer cacheHitLevel;
 
     public QueryExecutionViewMatcherMaster(
-    		Query query,
-    		QueryExecutionFactory subFactory,
-    		OpRewriteViewMatcherStateful opRewriter,
-    		ExecutorService executorService
+            Query query,
+            QueryExecutionFactory subFactory,
+            OpRewriteViewMatcherStateful opRewriter,
+            ExecutorService executorService
     ) {
-    	super(query, subFactory);
+        super(query, subFactory);
 
-    	this.opRewriter = opRewriter;
-    	this.context = ARQ.getContext();
-    	this.executorService = executorService;
+        this.opRewriter = opRewriter;
+        this.context = ARQ.getContext();
+        this.executorService = executorService;
     }
 
 
     public static ResultSetCloseable createResultSet(List<String> varNames, RangedSupplier<Long, Binding> rangedSupplier, Range<Long> range, Map<Var, Var> varMap) {
-    	Stream<Binding> stream = rangedSupplier.apply(range);
-    	if(varMap != null) {
-    		stream = stream.map(b -> BindingUtils.rename(b, varMap));
-    	}
+        Stream<Binding> stream = rangedSupplier.apply(range);
+        if(varMap != null) {
+            stream = stream.map(b -> BindingUtils.rename(b, varMap));
+        }
 
-    	ResultSet rs = ResultSetUtils.create(varNames, stream.iterator());
-    	ResultSetCloseable result = new ResultSetCloseable(rs);
+        ResultSet rs = ResultSetUtils.create(varNames, stream.iterator());
+        ResultSetCloseable result = new ResultSetCloseable(rs);
 
-    	return result;
+        return result;
     }
 
     /**
@@ -103,204 +105,211 @@ public class QueryExecutionViewMatcherMaster
      */
 
     public static boolean isView(Op op) {
-    	boolean result = op instanceof OpService && isView(((OpService)op).getService());
-    	return result;
+        boolean result = op instanceof OpService && isView(((OpService)op).getService());
+        return result;
     }
 
     public static boolean isView(Node node) {
-    	boolean result = isView(node.getURI());
-    	return result;
+        boolean result = isView(node.getURI());
+        return result;
     }
 
     public static boolean isView(String uri) {
-    	boolean result = uri.startsWith("view://");
-    	return result;
+        boolean result = uri.startsWith("view://");
+        return result;
     }
 
     @Override
     protected ResultSetCloseable executeCoreSelect(Query rawQuery) {
 
-    	Stopwatch sw = Stopwatch.createStarted();
+        Stopwatch sw = Stopwatch.createStarted();
 
-    	boolean cacheWholeQuery = true; //!rootService.getURI().startsWith("view://");
+        boolean cacheWholeQuery = true; //!rootService.getURI().startsWith("view://");
 
-    	List<Var> projectVars = rawQuery.getProjectVars();
-    	//List<String> projectVarNames = VarUtils.getVarNames(projectVars);
+        List<Var> projectVars = rawQuery.getProjectVars();
+        //List<String> projectVarNames = VarUtils.getVarNames(projectVars);
 
-    	// Store a present slice, but remove it from the query
-    	// NOTE This could also be done on the OP level; but
-    	// it seems to be much more convenient to be done on the query level
-    	Range<Long> range = QueryUtils.toRange(query);
-    	Query q = query.cloneQuery();
-    	q.setLimit(Query.NOLIMIT);
-    	q.setOffset(Query.NOLIMIT);
+        // Store a present slice, but remove it from the query
+        // NOTE This could also be done on the OP level; but
+        // it seems to be much more convenient to be done on the query level
+        Range<Long> range = QueryUtils.toRange(query);
+        Query q = query.cloneQuery();
+        q.setLimit(Query.NOLIMIT);
+        q.setOffset(Query.NOLIMIT);
 
-    	Op queryOp = Algebra.toQuadForm(Algebra.compile(q));
+        Op queryOp = Algebra.toQuadForm(Algebra.compile(q));
 //    	queryOp = SparqlViewMatcherOpImpl.normalizeOp(queryOp);
 
 
-    	// TODO opRewriter.lookup and opRewriter.put() both perform normalization
-    	// We could this duplicate processing by normalizing here
-    	// and passing the projected op to both functions
+        // TODO opRewriter.lookup and opRewriter.put() both perform normalization
+        // We could this duplicate processing by normalizing here
+        // and passing the projected op to both functions
         ProjectedOp pop = SparqlCacheUtils.cutProjectionAndNormalize(queryOp, SparqlViewMatcherOpImpl::normalizeOp);
+        List<Var> popVars = new ArrayList<>(pop.getProjection().getProjectVars());
+
         //Op coreQueryOp = pop.getResidualOp();
 
 
-    	// The thing here is, that in general we need to
-    	// - Initialize the execution context / jena-wise global data
-    	// - Perform the rewrite (may affect execution context state)
-    	// - Clean up the execution context / jena-wise global data
-    	RewriteResult2 rr = opRewriter.rewrite(pop);
-    	cacheHitLevel = rr.getRewriteLevel();
-    	Op rewrittenOp = rr.getOp();
+        // The thing here is, that in general we need to
+        // - Initialize the execution context / jena-wise global data
+        // - Perform the rewrite (may affect execution context state)
+        // - Clean up the execution context / jena-wise global data
+        RewriteResult2 rr = opRewriter.rewrite(pop);
+        cacheHitLevel = rr.getRewriteLevel();
+        Op rewrittenOp = rr.getOp();
 
 
-    	Map<Node, StorageEntry> storageMap = rr.getIdToStorageEntry();
+        Map<Node, StorageEntry> storageMap = rr.getIdToStorageEntry();
 
-    	// All subtrees that are to be executed on the original data source must be wrapped with
-		// a standard sparql service clause
+        // All subtrees that are to be executed on the original data source must be wrapped with
+        // a standard sparql service clause
 
-		Tree<Op> tree = OpUtils.createTree(rewrittenOp);
+        Tree<Op> tree = OpUtils.createTree(rewrittenOp);
 
-		// Find all referenced views in the expression
+        // Find all referenced views in the expression
 
-		Set<Node> cacheRefs = TreeUtils.inOrderSearch(tree.getRoot(), tree::getChildren)
-			.filter(QueryExecutionViewMatcherMaster::isView)
-			.map(op -> ((OpService)op).getService())
-			.collect(Collectors.toSet());
+        Set<Node> cacheRefs = TreeUtils.inOrderSearch(tree.getRoot(), tree::getChildren)
+            .filter(QueryExecutionViewMatcherMaster::isView)
+            .map(op -> ((OpService)op).getService())
+            .collect(Collectors.toSet());
 
-    	Cache<Node, StorageEntry> cache = opRewriter.getCache();
+        Cache<Node, StorageEntry> cache = opRewriter.getCache();
 
-		for(Node cacheRef : cacheRefs) {
-			StorageEntry e = cache.getIfPresent(cacheRef);
-			//RangedSupplier<Long, Binding> s = e.storage;
+        for(Node cacheRef : cacheRefs) {
+            StorageEntry e = cache.getIfPresent(cacheRef);
+            //RangedSupplier<Long, Binding> s = e.storage;
 
-			storageMap.put(cacheRef, e);
-		}
-
-
-		// Reverse the levels, so that we start with the leafs
-		Predicate<Op> predicate = x -> !(x instanceof OpService);
-
-		Set<Op> taggedNodes = TreeUtils.propagateBottomUpLabel(tree, predicate);
-
-		logger.debug("Tagged: " + taggedNodes);
-
-		// If we tagged the root node, then everything can be executed on the original service
+            storageMap.put(cacheRef, e);
+        }
 
 
-		int idX = 0;
-		// Remap all tagged nodes to be executed on the original service
-		Map<Op, Op> taggedToService = new IdentityHashMap<>();
+        // Reverse the levels, so that we start with the leafs
+        Predicate<Op> predicate = x -> !(x instanceof OpService);
 
-		// Track whether we created a new service for the root node
-		Node newRootServiceNode = null;
+        Set<Op> taggedNodes = TreeUtils.propagateBottomUpLabel(tree, predicate);
 
+        logger.debug("Tagged: " + taggedNodes);
 
-		for(Op tag : taggedNodes) {
-
-
-			boolean isRoot = tag == tree.getRoot();
-
-			// Do not cache pattern free queries
-			// TODO It would be better to decide caching based on the actual query execution time
-			// I.e. having an auto-caching layer would be nice
-			boolean isPatternFree = OpUtils.isPatternFree(tag);
-			if(isRoot && isPatternFree) {
-				cacheWholeQuery = false;
-			}
-
-			Node serviceNode;
-			if(isRoot && cacheWholeQuery) {
-	    		serviceNode = NodeFactory.createURI("view://ex.org/view" + pop.hashCode());
-				newRootServiceNode = serviceNode;
-			} else {
-				serviceNode = NodeFactory.createURI("view://service/" + idX++);
-			}
-
-			// We do not need to wrap parts of the query execution with a service
-			// if that part is pattern free (i.e. does not depend on external data)
-			// TODO Make sure that this works with EXISTS
-			if(!isPatternFree) {
-				Op serviceOp = new OpService(serviceNode, OpNull.create(), false);
-
-				//TransformDisjunctionToUnion
-				//tag = Transformer.transform(TransformDisjunctionToUnion.fn, tag);
-				Op execOp = SparqlViewMatcherOpImpl.denormalizeOp(tag);
-
-				Query qq = OpAsQuery.asQuery(execOp);
-
-				logger.info("Root query:\n" + qq);
-
-				RangedSupplier<Long, Binding> s3 = new RangedSupplierQuery(parentFactory, qq);
-
-				VarInfo varInfo = new VarInfo(new HashSet<>(qq.getProjectVars()), 0);
-				StorageEntry se = new StorageEntry(s3, varInfo); // The var info is not used
-				storageMap.put(serviceNode, se);
-
-				taggedToService.put(tag, serviceOp);
-			}
-		}
-
-		rewrittenOp = OpUtils.substitute(rewrittenOp, false, taggedToService::get);
-
-    	logger.debug("Raw query being rewritten for execution:\n" + rawQuery);
-    	logger.debug("Rewritten op being passed to execution:\n" + rewrittenOp);
+        // If we tagged the root node, then everything can be executed on the original service
 
 
-    	Context ctx = context.copy();
-    	ctx.put(OpExecutorViewCache.STORAGE_MAP, storageMap);
+        int idX = 0;
+        // Remap all tagged nodes to be executed on the original service
+        Map<Op, Op> taggedToService = new IdentityHashMap<>();
+
+        // Track whether we created a new service for the root node
+        Node newRootServiceNode = null;
 
 
-    	Set<Var> visibleVars = new HashSet<>(projectVars);//OpVars.visibleVars(rewrittenOp);
-    	VarInfo varInfo = new VarInfo(visibleVars, 0);
+        for(Op tag : taggedNodes) {
 
-    	RangedSupplier<Long, Binding> s2 = new RangedSupplierOp(rewrittenOp, ctx);
 
-    	if(cacheWholeQuery && newRootServiceNode != null) {
-    		// Caching the whole query requires the following actions:
-    		// (1) Allocate a new id for the query
-    		// (2) Create a storage entry for the rewritten entry
-    		// (3) Make the new id of the query together with its original (i.e. non-rewritten) op known to the rewriter
+            boolean isRoot = tag == tree.getRoot();
 
-    		//Node id = NodeFactory.createURI("view://ex.org/view" + queryOp.hashCode());
+            // Do not cache pattern free queries
+            // TODO It would be better to decide caching based on the actual query execution time
+            // I.e. having an auto-caching layer would be nice
+            boolean isPatternFree = OpUtils.isPatternFree(tag);
+            if(isRoot && isPatternFree) {
+                cacheWholeQuery = false;
+            }
 
-        	s2 = new RangedSupplierLazyLoadingListCache<Binding>(executorService, s2, Range.closedOpen(0l, 10000l));
-    		//s2 = new RangedSupplierLazyLoadingListCache<Binding>(executorService, s2, range);
+            Node serviceNode;
+            if(isRoot && cacheWholeQuery) {
+                serviceNode = NodeFactory.createURI("view://ex.org/view" + pop.hashCode());
+                newRootServiceNode = serviceNode;
+            } else {
+                serviceNode = NodeFactory.createURI("view://service/" + idX++);
+            }
 
-        	StorageEntry se2 = new StorageEntry(s2, varInfo);
+            // We do not need to wrap parts of the query execution with a service
+            // if that part is pattern free (i.e. does not depend on external data)
+            // TODO Make sure that this works with EXISTS
+            if(!isPatternFree) {
+                Op serviceOp = new OpService(serviceNode, OpNull.create(), false);
 
-        	// Update the storage entry with the cache wrapper
-    		//storageMap.put(newRootServiceNode, se2);
+                //TransformDisjunctionToUnion
+                //tag = Transformer.transform(TransformDisjunctionToUnion.fn, tag);
+                Op execOp = SparqlViewMatcherOpImpl.denormalizeOp(tag);
 
-        	// TODO The registration at the cache and the rewriter should be atomic
-        	// At least we need to deal with the chance that the rewriter maps an op to an id for
-        	// which the storageEntry has not yet been registered at the cache
-    		opRewriter.put(newRootServiceNode, pop);
-        	cache.put(newRootServiceNode, se2);
-    	}
+                // Append the prior removed projection
+                //execOp = new OpProject(execOp, popVars);
 
-    	List<String> visibleVarNames = VarUtils.getVarNames(visibleVars);
-    	ResultSetCloseable result = createResultSet(visibleVarNames, s2, range, null);
+                Query qq = OpAsQuery.asQuery(execOp);
+                qq.getProjectVars().clear();
+                qq.getProjectVars().addAll(popVars);
 
-    	preparationTimeInSec =  sw.stop().elapsed(TimeUnit.NANOSECONDS) / 1000000000.0;
+                logger.info("Root query:\n" + qq);
 
-    	System.out.println("Time to prepare the result set: " + (preparationTimeInSec * 1000) + " ms");
+                RangedSupplier<Long, Binding> s3 = new RangedSupplierQuery(parentFactory, qq);
 
-    	return result;
+                VarInfo varInfo = new VarInfo(new HashSet<>(qq.getProjectVars()), 0);
+                StorageEntry se = new StorageEntry(s3, varInfo); // The var info is not used
+                storageMap.put(serviceNode, se);
+
+                taggedToService.put(tag, serviceOp);
+            }
+        }
+
+        rewrittenOp = OpUtils.substitute(rewrittenOp, false, taggedToService::get);
+
+        logger.debug("Raw query being rewritten for execution:\n" + rawQuery);
+        logger.debug("Rewritten op being passed to execution:\n" + rewrittenOp);
+
+
+        Context ctx = context.copy();
+        ctx.put(OpExecutorViewCache.STORAGE_MAP, storageMap);
+
+
+        Set<Var> visibleVars = new HashSet<>(projectVars);//OpVars.visibleVars(rewrittenOp);
+        VarInfo varInfo = new VarInfo(visibleVars, 0);
+
+        RangedSupplier<Long, Binding> s2 = new RangedSupplierOp(rewrittenOp, ctx);
+
+        if(cacheWholeQuery && newRootServiceNode != null) {
+            // Caching the whole query requires the following actions:
+            // (1) Allocate a new id for the query
+            // (2) Create a storage entry for the rewritten entry
+            // (3) Make the new id of the query together with its original (i.e. non-rewritten) op known to the rewriter
+
+            //Node id = NodeFactory.createURI("view://ex.org/view" + queryOp.hashCode());
+
+            s2 = new RangedSupplierLazyLoadingListCache<Binding>(executorService, s2, Range.closedOpen(0l, 10000l));
+            //s2 = new RangedSupplierLazyLoadingListCache<Binding>(executorService, s2, range);
+
+            StorageEntry se2 = new StorageEntry(s2, varInfo);
+
+            // Update the storage entry with the cache wrapper
+            //storageMap.put(newRootServiceNode, se2);
+
+            // TODO The registration at the cache and the rewriter should be atomic
+            // At least we need to deal with the chance that the rewriter maps an op to an id for
+            // which the storageEntry has not yet been registered at the cache
+            opRewriter.put(newRootServiceNode, pop);
+            cache.put(newRootServiceNode, se2);
+        }
+
+        List<String> visibleVarNames = VarUtils.getVarNames(visibleVars);
+        ResultSetCloseable result = createResultSet(visibleVarNames, s2, range, null);
+
+        preparationTimeInSec =  sw.stop().elapsed(TimeUnit.NANOSECONDS) / 1000000000.0;
+
+        System.out.println("Time to prepare the result set: " + (preparationTimeInSec * 1000) + " ms");
+
+        return result;
     }
 
 
-	@Override
-	protected QueryExecution executeCoreSelectX(Query query) {
-		// TODO Fix bad design - this method is not needed
-		return null;
-	}
+    @Override
+    protected QueryExecution executeCoreSelectX(Query query) {
+        // TODO Fix bad design - this method is not needed
+        return null;
+    }
 
 
-	public Integer getCacheHitLevel() {
-		return cacheHitLevel;
-	}
+    public Integer getCacheHitLevel() {
+        return cacheHitLevel;
+    }
 
 }
 
@@ -358,8 +367,8 @@ public class QueryExecutionViewMatcherMaster
 
 
 
-	// For each parents of which all children are in the set, remove the children from the set
-	// and add the parent to the set instead
+    // For each parents of which all children are in the set, remove the children from the set
+    // and add the parent to the set instead
 
 
 
