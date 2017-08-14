852e36bac004149332bd462cb7ebd58978eba8de
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-08-14 20:49:59 +0200

    fixed the servlet

--- jena-sparql-api-algebra/src/main/java/org/aksw/jena_sparql_api/algebra/analysis/VarUsage.java
+++ jena-sparql-api-algebra/src/main/java/org/aksw/jena_sparql_api/algebra/analysis/VarUsage.java
@@ -8,51 +8,53 @@ import org.apache.jena.sparql.core.Var;
 import com.google.common.collect.Multimap;
 
 public class VarUsage {
-	protected Set<Var> referencedVars;
-	protected Set<Var> nonUnique;
-	protected Multimap<Var, Var> varDeps;
-	protected Set<Set<Var>> uniqueSets;
+    protected Set<Var> referencedVars;
+    protected Set<Var> nonUnique;
+    protected Multimap<Var, Var> varDeps;
+    protected Set<Set<Var>> uniqueSets;
 
-	public VarUsage(
-			Set<Var> referencedVars,
-			Set<Var> nonUnique,
-			Multimap<Var, Var> varDeps,
-			Set<Set<Var>> uniqueSets) {
-		super();
-		this.referencedVars = referencedVars;
-		this.nonUnique = nonUnique;
-		this.varDeps = varDeps;
-		this.uniqueSets = uniqueSets;
-	}
+    // newly defined vars
 
-	public Set<Var> getReferencedVars() {
-		return referencedVars;
-	}
+    public VarUsage(
+            Set<Var> referencedVars,
+            Set<Var> nonUnique,
+            Multimap<Var, Var> varDeps,
+            Set<Set<Var>> uniqueSets) {
+        super();
+        this.referencedVars = referencedVars;
+        this.nonUnique = nonUnique;
+        this.varDeps = varDeps;
+        this.uniqueSets = uniqueSets;
+    }
 
-	public Set<Var> getNonUnique() {
-		return nonUnique;
-	}
+    public Set<Var> getReferencedVars() {
+        return referencedVars;
+    }
 
-	public Multimap<Var, Var> getVarDeps() {
-		return varDeps;
-	}
+    public Set<Var> getNonUnique() {
+        return nonUnique;
+    }
 
-	public Set<Set<Var>> getUniqueSets() {
-		return uniqueSets;
-	}
+    public Multimap<Var, Var> getVarDeps() {
+        return varDeps;
+    }
 
-	@Override
-	public String toString() {
-		return "VarUsage[ proj:" + varDeps.keySet() + ", refs: " + referencedVars +
-				", deps: " + varDeps + ", non-uniq: " + nonUnique + ", uniq sets: " + uniqueSets + "]";
-	}
+    public Set<Set<Var>> getUniqueSets() {
+        return uniqueSets;
+    }
+
+    @Override
+    public String toString() {
+        return "VarUsage[ proj:" + varDeps.keySet() + ", refs: " + referencedVars +
+                ", deps: " + varDeps + ", non-uniq: " + nonUnique + ", uniq sets: " + uniqueSets + "]";
+    }
 
 
-	public static Set<Var> getMandatoryVars(VarUsage varUsage) {
-		Set<Var> result = new HashSet<Var>();
-		result.addAll(varUsage.referencedVars);
-		result.addAll(varUsage.varDeps.values());
-		return result;
-	}
+    public static Set<Var> getMandatoryVars(VarUsage varUsage) {
+        Set<Var> result = new HashSet<Var>();
+        result.addAll(varUsage.referencedVars);
+        result.addAll(varUsage.varDeps.values());
+        return result;
+    }
 
 }
