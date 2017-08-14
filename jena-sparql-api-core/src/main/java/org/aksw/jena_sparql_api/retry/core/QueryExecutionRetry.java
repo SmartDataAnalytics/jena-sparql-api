a7f7c445e09f0ae70b96dc779805560f6f4c865d
Simon Bin <sbin@informatik.uni-leipzig.de>
2017-08-14 23:15:24 +0200

    implement retry (initial try)
    todo: write test

--- jena-sparql-api-core/src/main/java/org/aksw/jena_sparql_api/retry/core/QueryExecutionRetry.java
+++ jena-sparql-api-core/src/main/java/org/aksw/jena_sparql_api/retry/core/QueryExecutionRetry.java
@@ -1,23 +1,23 @@
 package org.aksw.jena_sparql_api.retry.core;
 
-import java.util.Iterator;
-import java.util.concurrent.Callable;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.function.Supplier;
-
-import org.aksw.jena_sparql_api.core.QueryExecutionDecorator;
-import org.apache.jena.graph.Triple;
-import org.apache.jena.query.QueryExecution;
-import org.apache.jena.query.ResultSet;
-import org.apache.jena.rdf.model.Model;
-
 import com.google.common.util.concurrent.ListenableFuture;
 import com.nurkiewicz.asyncretry.AsyncRetryExecutor;
 import com.nurkiewicz.asyncretry.RetryExecutor;
 import com.nurkiewicz.asyncretry.backoff.Backoff;
 import com.nurkiewicz.asyncretry.policy.AbortRetryException;
 import com.nurkiewicz.asyncretry.policy.RetryPolicy;
+import org.aksw.jena_sparql_api.core.QueryExecutionDecorator;
+import org.apache.jena.graph.Triple;
+import org.apache.jena.query.*;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.sparql.core.Quad;
+import org.apache.jena.sparql.util.Context;
+
+import java.util.Iterator;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 
 public class QueryExecutionRetry
 	extends QueryExecutionDecorator
@@ -28,48 +28,49 @@ public class QueryExecutionRetry
 	protected RetryPolicy retryPolicy;
 	protected Backoff backoff;
 	protected boolean fixedDelay;
-	protected ScheduledExecutorService scheduler;
+	protected Supplier<ScheduledExecutorService> scheduler;
 
 	protected boolean aborted = false;
+	private QueryExecutionRetryDecorateeProxy proxy;
 
-	public QueryExecutionRetry(Supplier<QueryExecution> supplier, int retryCount, long retryDelayInMs, ScheduledExecutorService scheduler) {
-		super(null);
-		this.supplier = supplier;
-		this.retryCount = retryCount;
-		this.retryDelayInMs = retryDelayInMs;
-		this.scheduler = scheduler;
-	}
-
-	public QueryExecutionRetry(Supplier<QueryExecution> supplier, RetryPolicy retryPolicy, Backoff backoff, boolean fixedDelay, ScheduledExecutorService scheduler) {
-		super(null);
+	public QueryExecutionRetry(Supplier<QueryExecution> supplier, RetryPolicy retryPolicy, Backoff backoff, boolean fixedDelay, Supplier<ScheduledExecutorService> scheduler) {
+		super(new QueryExecutionRetryDecorateeProxy());
 
 		this.supplier = supplier;
 		this.retryPolicy = retryPolicy;
 		this.backoff = backoff;
 		this.fixedDelay = fixedDelay;
 		this.scheduler = scheduler;
+		((QueryExecutionRetryDecorateeProxy) this.decoratee).setOwner(this);
 	}
 
+	public <T> T resolve(Callable<T> callable) {
+		if(decoratee instanceof QueryExecutionRetryDecorateeProxy) {
+			this.proxy = (QueryExecutionRetryDecorateeProxy) decoratee;
+			decoratee = supplier.get();
+			if (proxy.getInitialBinding() != null)
+				decoratee.setInitialBinding(proxy.getInitialBinding());
+			if (proxy.getTimeout1() > -1L || proxy.getTimeout2() > -1L)
+				decoratee.setTimeout(proxy.getTimeout1(), proxy.getTimeout2());
+		}
+		try {
+			return callable.call();
+		} catch(Exception e) {
+			if(aborted) {
+				throw new AbortRetryException();
+			}
+
+			decoratee = this.proxy;
+			this.proxy = null;
+			throw new RuntimeException(e);
+		}
+	}
 
 	public <T> T doTry(Callable<T> callable) {
-		Callable<T> wrapper = () -> {
-			if(decoratee == null) {
-				decoratee = supplier.get();
-			}
-			try {
-				return callable.call();
-			} catch(Exception e) {
-				if(aborted) {
-					throw new AbortRetryException();
-				}
+		Callable<T> wrapper = () -> resolve(callable);
 
-				decoratee = null;
-				throw new RuntimeException(e);
-			}
-		};
-
-		//ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
-		RetryExecutor executor = new AsyncRetryExecutor(scheduler, retryPolicy, backoff, fixedDelay);
+		ScheduledExecutorService service = scheduler.get();
+		RetryExecutor executor = new AsyncRetryExecutor(service, retryPolicy, backoff, fixedDelay);
 
 		ListenableFuture<T> future = executor.getWithRetry(wrapper);
 //		scheduler.shutdown();
@@ -79,7 +80,7 @@ public class QueryExecutionRetry
 		} catch (Exception e) {
 			throw new RuntimeException("Query Execution failed, even with retries.", e);
 		} finally {
-			scheduler.shutdown();
+			service.shutdown();
 		}
 
 //		CallableRetry<T> retry = new CallableRetry<T>(callable, retryCount, retryDelayInMs);
@@ -139,5 +140,150 @@ public class QueryExecutionRetry
 	@Override
 	public Iterator<Triple> execDescribeTriples() {
 		return doTry(() -> super.execDescribeTriples());
+	}
+
+	private static class QueryExecutionRetryDecorateeProxy implements QueryExecution {
+		private QueryExecutionRetry owner;
+		private QuerySolution initialBinding;
+		private long timeout1 = -1L;
+		private long timeout2 = -1L;
+
+		@Override
+		public void setInitialBinding(QuerySolution querySolution) {
+			this.initialBinding = querySolution;
+		}
+
+		public QuerySolution getInitialBinding() {
+			return initialBinding;
+		}
+
+		@Override
+		public Dataset getDataset() {
+			return owner.resolve(owner::getDataset);
+		}
+
+		@Override
+		public Context getContext() {
+			return owner.resolve(owner::getContext);
+		}
+
+		@Override
+		public Query getQuery() {
+			return owner.resolve(owner::getQuery);
+		}
+
+		@Override
+		public ResultSet execSelect() {
+			return owner.execSelect();
+		}
+
+		@Override
+		public Model execConstruct() {
+			return owner.execConstruct();
+		}
+
+		@Override
+		public Model execConstruct(Model model) {
+			return owner.execConstruct(model);
+		}
+
+		@Override
+		public Iterator<Triple> execConstructTriples() {
+			return owner.execConstructTriples();
+		}
+
+		@Override
+		public Iterator<Quad> execConstructQuads() {
+			return owner.resolve(owner::execConstructQuads);
+		}
+
+		@Override
+		public Dataset execConstructDataset() {
+			return owner.resolve(owner::execConstructDataset);
+		}
+
+		@Override
+		public Dataset execConstructDataset(Dataset dataset) {
+			return owner.resolve(() -> owner.execConstructDataset(dataset));
+		}
+
+		@Override
+		public Model execDescribe() {
+			return owner.execDescribe();
+		}
+
+		@Override
+		public Model execDescribe(Model model) {
+			return owner.execDescribe(model);
+		}
+
+		@Override
+		public Iterator<Triple> execDescribeTriples() {
+			return owner.execDescribeTriples();
+		}
+
+		@Override
+		public boolean execAsk() {
+			return owner.execAsk();
+		}
+
+		@Override
+		public void abort() {
+			owner.abort();
+		}
+
+		@Override
+		public void close() {
+			owner.close();
+		}
+
+		@Override
+		public boolean isClosed() {
+			return owner.isClosed();
+		}
+
+		public void setTimeout(long timeout, TimeUnit timeUnit) {
+			long x = asMillis(timeout, timeUnit);
+			this.timeout1 = -1L;
+			this.timeout2 = x;
+		}
+
+		public void setTimeout(long timeout) {
+			this.setTimeout(timeout, TimeUnit.MILLISECONDS);
+		}
+
+		public void setTimeout(long timeout1, TimeUnit timeUnit1, long timeout2, TimeUnit timeUnit2) {
+			long x1 = asMillis(timeout1, timeUnit1);
+			long x2 = asMillis(timeout2, timeUnit2);
+			this.timeout1 = x1;
+			if (timeout2 < 0L) {
+				this.timeout2 = -1L;
+			} else {
+				this.timeout2 = x2;
+			}
+
+		}
+
+		public void setTimeout(long timeout1, long timeout2) {
+			this.setTimeout(timeout1, TimeUnit.MILLISECONDS, timeout2, TimeUnit.MILLISECONDS);
+		}
+
+		private static long asMillis(long duration, TimeUnit timeUnit) {
+			return duration < 0L ? duration : timeUnit.toMillis(duration);
+		}
+
+		@Override
+		public long getTimeout1() {
+			return this.timeout1;
+		}
+
+		@Override
+		public long getTimeout2() {
+			return this.timeout2;
+		}
+
+		public void setOwner(QueryExecutionRetry owner) {
+			this.owner = owner;
+		}
 	}
 }
