e1cebd57717f61ec9f08e4be9f6034e270b80f60
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-08-28 18:23:35 +0200

    more work on the index

--- jena-sparql-api-query-containment/src/main/java/org/aksw/jena_sparql_api/query_containment/index/QueryContainmentIndexImpl.java
+++ jena-sparql-api-query-containment/src/main/java/org/aksw/jena_sparql_api/query_containment/index/QueryContainmentIndexImpl.java
@@ -1,15 +1,21 @@
 package org.aksw.jena_sparql_api.query_containment.index;
 
+import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.function.Function;
 
 import org.aksw.commons.collections.trees.Tree;
+import org.aksw.commons.collections.trees.TreeImpl;
+import org.aksw.commons.collections.trees.TreeNode;
+import org.aksw.commons.collections.trees.TreeNodeImpl;
 import org.aksw.commons.collections.trees.TreeUtils;
 import org.aksw.commons.graph.index.core.SubgraphIsomorphismIndex;
 import org.aksw.commons.graph.index.jena.SubgraphIsomorphismIndexJena;
-import org.aksw.commons.graph.index.jena.transform.OpTransformNormalizeUnaryOps;
 import org.aksw.commons.graph.index.jena.transform.QueryToGraph;
 import org.aksw.commons.jena.jgrapht.PseudoGraphJenaGraph;
+import org.aksw.jena_sparql_api.algebra.utils.OpExtConjunctiveQuery;
 import org.aksw.jena_sparql_api.algebra.utils.OpUtils;
 import org.apache.jena.graph.Graph;
 import org.apache.jena.graph.Node;
@@ -17,30 +23,42 @@ import org.apache.jena.graph.Triple;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.sparql.algebra.Algebra;
 import org.apache.jena.sparql.algebra.Op;
-import org.apache.jena.sparql.algebra.Transformer;
 import org.jgrapht.DirectedGraph;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
 
 
 
 public class QueryContainmentIndexImpl<K, G, N, O> {
 
     protected Function<? super O, O> normalizer;
-    protected Function<? super O, G> queryToGraph;
+    protected Function<? super O, G> opToGraph;
 
-    protected SubgraphIsomorphismIndex<K, G, N> index;
+    protected Function<O, List<O>> parentToChildren;
+
+    protected SubgraphIsomorphismIndex<Entry<K, Long>, G, N> index;
+
+    protected Table<K, Long, TreeNode<O>> keyToNodeIndexToNode = HashBasedTable.create();
 
     public static DirectedGraph<Node, Triple> queryToJGraphT(Op op) {
-        Graph jenaGraph = QueryToGraph.queryToGraph(op);
-        DirectedGraph<Node, Triple> result = new PseudoGraphJenaGraph(jenaGraph);
+        DirectedGraph<Node, Triple> result = null;
+
+        if(op instanceof OpExtConjunctiveQuery) {
+            Graph jenaGraph = QueryToGraph.queryToGraph(op);
+            result = new PseudoGraphJenaGraph(jenaGraph);
+        }
+
         return result;
     }
 
     public static <K> QueryContainmentIndexImpl<K, DirectedGraph<Node, Triple>, Node, Op> create() {
 
-        SubgraphIsomorphismIndex<K, DirectedGraph<Node, Triple>, Node> sii = SubgraphIsomorphismIndexJena.create();
+        SubgraphIsomorphismIndex<Entry<K, Long>, DirectedGraph<Node, Triple>, Node> sii = SubgraphIsomorphismIndexJena.create();
 
         QueryContainmentIndexImpl<K, DirectedGraph<Node, Triple>, Node, Op> result = new QueryContainmentIndexImpl<K, DirectedGraph<Node, Triple>, Node, Op>(
                 QueryToGraph::normalizeOp,
+                OpUtils::getSubOps,
                 QueryContainmentIndexImpl::queryToJGraphT,
                 sii
                 );
@@ -49,12 +67,29 @@ public class QueryContainmentIndexImpl<K, G, N, O> {
     }
 
 
-    public QueryContainmentIndexImpl(Function<? super O, O> normalizer, Function<? super O, G> queryToGraph,
-            SubgraphIsomorphismIndex<K, G, N> index) {
+    public QueryContainmentIndexImpl(
+            Function<? super O, O> normalizer,
+            Function<O, List<O>> parentToChildren,
+            Function<? super O, G> opToGraph,
+            SubgraphIsomorphismIndex<Entry<K, Long>, G, N> index) {
         super();
         this.normalizer = normalizer;
-        this.queryToGraph = queryToGraph;
+        this.parentToChildren = parentToChildren;
+        this.opToGraph = opToGraph;
         this.index = index;
+    }
+
+
+    public void remove(K key) {
+        Map<Long, TreeNode<O>> rows = keyToNodeIndexToNode.row(key);
+
+        // Remove related index entries
+        for(Long id : rows.keySet()) {
+            Entry<K, Long> e = new SimpleEntry<>(key, id);
+            index.removeKey(e);
+        }
+
+        rows.clear();
     }
 
     public void put(K key, O viewOp) {
@@ -62,8 +97,38 @@ public class QueryContainmentIndexImpl<K, G, N, O> {
 
         O normViewOp = normalizer.apply(viewOp);
 
-        Tree<Op> tree = OpUtils.createTree((Op)normViewOp);
-        List<List<Op>> nodesPerLevel = TreeUtils.nodesPerLevel(tree);
+        Tree<O> tree = TreeImpl.create(normViewOp, parentToChildren);//OpUtils.createTree((Op)normViewOp);
+        //TreeNodeImpl<Op>
+
+
+//        List<List<Op>> nodesPerLevel = TreeUtils.nodesPerLevel(tree);
+//        List<List<TreeNode<Op>>> npl = nodesPerLevel.stream()
+//                .map(l -> l.stream().map(n -> (TreeNode<Op>)new TreeNodeImpl<Op>(tree, n)).collect(Collectors.toList()))
+//                .collect(Collectors.toList());
+//
+//        Collections.reverse(npl);
+
+//        npl.get(0).forEach(x -> System.out.println("Item: " + x));
+
+
+        // Convert the leaf nodes to graphs
+        long leafNodeId[] = {0};
+
+        TreeUtils.inOrderSearch(tree.getRoot(), tree::getChildren).forEach(op -> {
+            TreeNode<O> node = new TreeNodeImpl<>(tree, op);
+
+            G graph = opToGraph.apply(op);
+            if(graph != null) {
+                Entry<K, Long> e = new SimpleEntry<>(key, leafNodeId[0]);
+
+                keyToNodeIndexToNode.put(key, leafNodeId[0], node);
+                index.put(e, graph);
+
+                leafNodeId[0]++;
+            }
+        });
+
+
 
         // Iterate the leaf nodes
     }
@@ -111,11 +176,6 @@ public class QueryContainmentIndexImpl<K, G, N, O> {
         QueryContainmentIndexImpl<Node, DirectedGraph<Node, Triple>, Node, Op> index = QueryContainmentIndexImpl.create();
         Op op = Algebra.toQuadForm(Algebra.compile(QueryFactory.create("PREFIX : <http://ex.org/> SELECT ?s { { SELECT DISTINCT ?s { ?s a ?t . FILTER(?t = :foo || ?t = :bar) } } UNION { ?x ?y ?z } }")));
 
-        System.out.println(op);
-        op = QueryToGraph.normalizeOp(op);
-//        op = Transformer.transform(new OpTransformNormalizeUnaryOps(), op);
-
-        System.out.println("normalized args: " + op);
 
         index.put(Node.ANY, op);
 
