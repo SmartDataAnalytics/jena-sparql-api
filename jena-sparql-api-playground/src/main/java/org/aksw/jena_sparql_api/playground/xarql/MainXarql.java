baa3625d6bd52f3e10de4fd7929e03d1caebf465
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2017-11-15 22:53:39 +0100

    updated for SDA

--- jena-sparql-api-playground/src/main/java/org/aksw/jena_sparql_api/playground/xarql/MainXarql.java
+++ jena-sparql-api-playground/src/main/java/org/aksw/jena_sparql_api/playground/xarql/MainXarql.java
@@ -1,30 +1,32 @@
-package org.aksw.jena_sparql_api.batch.cli.main;
+package org.aksw.jena_sparql_api.playground.xarql;
 
-import java.io.IOException;
+import java.awt.Desktop;
+import java.net.URI;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.function.Function;
 
 import org.aksw.jena_sparql_api.core.FluentQueryExecutionFactory;
 import org.aksw.jena_sparql_api.core.QueryExecutionFactory;
+import org.aksw.jena_sparql_api.server.utils.FactoryBeanSparqlServer;
 import org.aksw.jena_sparql_api.sparql.ext.csv.JenaExtensionCsv;
 import org.aksw.jena_sparql_api.sparql.ext.json.JenaExtensionJson;
 import org.aksw.jena_sparql_api.sparql.ext.util.JenaExtensionUtil;
 import org.aksw.jena_sparql_api.sparql.ext.xml.JenaExtensionXml;
+import org.aksw.jena_sparql_api.stmt.SparqlStmt;
 import org.aksw.jena_sparql_api.stmt.SparqlStmtParserImpl;
-import org.apache.jena.query.QueryExecution;
 import org.apache.jena.query.Syntax;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
-import org.apache.jena.riot.RDFDataMgr;
-import org.apache.jena.riot.RDFFormat;
 import org.apache.jena.shared.PrefixMapping;
 import org.apache.jena.shared.impl.PrefixMappingImpl;
 import org.apache.jena.vocabulary.RDFS;
+import org.eclipse.jetty.server.Server;
 
 public class MainXarql {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		//JenaExtensionJson.register();
 		
 		Path path = Paths.get(args[0]); //"/home/raven/Projects/Eclipse/trento-bike-racks/datasets/bikesharing/trento-bike-sharing.json");
@@ -46,17 +48,35 @@ public class MainXarql {
 		
 		String q = args[1];
 		
+		Function<String, SparqlStmt> sparqlStmtParser = SparqlStmtParserImpl.create(Syntax.syntaxSPARQL_11, pm, false);//.getQueryParser();
+		
 		QueryExecutionFactory qef = FluentQueryExecutionFactory.from(model)
 				.config()
-					.withParser(SparqlStmtParserImpl.create(Syntax.syntaxSPARQL_11, pm, false).getQueryParser())
+					//.withParser(sparqlStmtParser)
 					//.withPrefixes(PrefixMapping.Extended, false)
 					.end().create();
-		try(QueryExecution qe = qef.createQueryExecution(q)) {
-			Model result = qe.execConstruct();
-			RDFDataMgr.write(System.out, result, RDFFormat.TURTLE_PRETTY);
-			//ResultSet rs = qe.execSelect();
-			//System.out.println(ResultSetFormatter.asText(rs));
-		} 
+
+		int port = 7532;
+		Server server = FactoryBeanSparqlServer.newInstance()
+			.setSparqlServiceFactory(qef)
+			.setSparqlStmtParser(sparqlStmtParser)
+			.setPort(port)
+			.create();
+
+		server.start();
+		
+		if(Desktop.isDesktopSupported()) {
+			Desktop.getDesktop().browse(new URI("http://localhost:" + port + "/sparql"));
+		}
+
+		server.join();
+		
+//		try(QueryExecution qe = qef.createQueryExecution(q)) {
+//			Model result = qe.execConstruct();
+//			RDFDataMgr.write(System.out, result, RDFFormat.TURTLE_PRETTY);
+//			//ResultSet rs = qe.execSelect();
+//			//System.out.println(ResultSetFormatter.asText(rs));
+//		} 
 	}
 	
 }
