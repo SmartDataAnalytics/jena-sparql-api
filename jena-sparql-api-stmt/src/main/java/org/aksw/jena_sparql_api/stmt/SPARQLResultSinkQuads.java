3872a8269db87bcc37d5e11c22c1d32b45b22271
Claus Stadler <cstadler@informatik.uni-leipzig.de>
2019-12-05 15:25:49 +0100

    Made sparql stmt sinks configurable

--- jena-sparql-api-stmt/src/main/java/org/aksw/jena_sparql_api/stmt/SPARQLResultSinkQuads.java
+++ jena-sparql-api-stmt/src/main/java/org/aksw/jena_sparql_api/stmt/SPARQLResultSinkQuads.java
@@ -4,34 +4,39 @@ import java.util.function.Consumer;
 
 import org.apache.jena.atlas.lib.Sink;
 import org.apache.jena.query.ResultSet;
-import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.sparql.core.Quad;
-
-import com.google.gson.Gson;
 
 public class SPARQLResultSinkQuads
 	implements SPARQLResultSink {
 	
 	//protected Consumer<Quad> consumer;
 	protected Sink<Quad> sink;
-	protected Gson gson;
+	protected SPARQLResultSink fallback;
+
+	public SPARQLResultSinkQuads(Consumer<Quad> sink) {
+		this(sink, null);
+	}
+
+	public SPARQLResultSinkQuads(Consumer<Quad> sink, SPARQLResultSink fallback) {
+		this(new Sink<Quad>() {
+			@Override public void send(Quad item) { sink.accept(item);}
+			@Override public void close() { }
+			@Override public void flush() { }
+		}, fallback);
+	}
+
+//	public SPARQLResultSinkQuads(Sink<Quad> sink, SPARQLResultSink fallback) {
+//		this(sink, fallback, null);
+//	}
 
 	public SPARQLResultSinkQuads(Sink<Quad> sink) {
 		this(sink, null);
 	}
 
-	public SPARQLResultSinkQuads(Consumer<Quad> sink) {
-		this(new Sink<Quad>() {
-			@Override public void send(Quad item) { sink.accept(item);}
-			@Override public void close() { }
-			@Override public void flush() { }
-		}, null);
-	}
-
-	public SPARQLResultSinkQuads(Sink<Quad> sink, Gson gson) {
+	public SPARQLResultSinkQuads(Sink<Quad> sink, SPARQLResultSink fallback) {
 		super();
 		this.sink = sink;
-		this.gson = gson != null ? gson : new Gson();
+		this.fallback = fallback != null ? fallback : new SPARQLResultSinkPrint(System.err);
 	}
 
 	@Override
@@ -41,14 +46,16 @@ public class SPARQLResultSinkQuads
 
 	@Override
 	public void onResultSet(ResultSet rs) {
-		String str = ResultSetFormatter.asText(rs);
-		System.err.println(str);
+		fallback.onResultSet(rs);
+//		String str = ResultSetFormatter.asText(rs);
+//		System.err.println(str);
 	}
 	
 	@Override
 	public void onJson(com.google.gson.JsonElement value) {
-		String json = gson.toJson(value);
-		System.err.println(json);
+		fallback.onJson(value);
+//		String json = gson.toJson(value);
+//		System.err.println(json);
 	}
 
 	@Override
