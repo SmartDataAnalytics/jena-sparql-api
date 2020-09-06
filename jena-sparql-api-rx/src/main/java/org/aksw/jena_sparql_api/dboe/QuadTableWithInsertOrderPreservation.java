e4ae89ba6f1b36bc17109fec2816254e87ff1395
Claus Stadler <RavenArkadon@googlemail.com>
2020-09-06 16:19:56 +0200

    changed collection to set as it meets the interface contract better

--- jena-sparql-api-rx/src/main/java/org/aksw/jena_sparql_api/dboe/QuadTableWithInsertOrderPreservation.java
+++ jena-sparql-api-rx/src/main/java/org/aksw/jena_sparql_api/dboe/QuadTableWithInsertOrderPreservation.java
@@ -1,7 +1,7 @@
 package org.aksw.jena_sparql_api.dboe;
 
-import java.util.Collection;
 import java.util.LinkedHashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 
 import org.apache.jena.graph.Node;
@@ -18,41 +18,41 @@ public class QuadTableWithInsertOrderPreservation
     implements QuadTableCore
 {
     protected QuadTableCore delegate;
-    protected Collection<Quad> collection;
+    protected Set<Quad> set;
 
     public QuadTableWithInsertOrderPreservation(QuadTableCore delegate) {
         this(delegate, new LinkedHashSet<>());
     }
 
-    public QuadTableWithInsertOrderPreservation(QuadTableCore delegate, Collection<Quad> collection) {
+    public QuadTableWithInsertOrderPreservation(QuadTableCore delegate, Set<Quad> collection) {
         super();
         this.delegate = delegate;
-        this.collection = collection;
+        this.set = collection;
     }
 
     @Override
     public void clear() {
         delegate.clear();
-        collection.clear();
+        set.clear();
     }
 
     @Override
     public void add(Quad quad) {
         delegate.add(quad);
-        collection.add(quad);
+        set.add(quad);
     }
 
     @Override
     public void delete(Quad quad) {
         delegate.delete(quad);
-        collection.remove(quad);
+        set.remove(quad);
     }
 
     @Override
     public Stream<Quad> find(Node g, Node s, Node p, Node o) {
         boolean matchesAny = Node.ANY.matches(g) && Node.ANY.matches(s) && Node.ANY.matches(p) && Node.ANY.matches(o);
         Stream<Quad> result = matchesAny
-                ? collection.stream()
+                ? set.stream()
                 : delegate.find(g, s, p, o);
         return result;
     }
