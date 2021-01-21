97949fa2c80657e9fd5df1537a695c9fd4d7f916
Claus Stadler <RavenArkadon@googlemail.com>
2021-01-21 13:17:55 +0100

    Improved decision tree model

--- jena-sparql-api-concepts/src/main/java/org/aksw/jena_sparql_api/decision_tree/api/DecisionTree.java
+++ jena-sparql-api-concepts/src/main/java/org/aksw/jena_sparql_api/decision_tree/api/DecisionTree.java
@@ -2,24 +2,37 @@ package org.aksw.jena_sparql_api.decision_tree.api;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.Map;
-import java.util.Set;
 
 import org.aksw.jena_sparql_api.utils.Vars;
 import org.apache.jena.ext.com.google.common.collect.Iterables;
 import org.apache.jena.graph.Node;
 import org.apache.jena.graph.NodeFactory;
+import org.apache.jena.sparql.core.Var;
 import org.apache.jena.sparql.engine.binding.Binding;
 import org.apache.jena.sparql.engine.binding.BindingFactory;
 import org.apache.jena.sparql.expr.Expr;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.util.ExprUtils;
 
+import com.google.common.base.Optional;
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+
+
 interface DtNode<C, V, T> {
 	DtNode<C, V, T> getParent();
 	V getReachingValue();
+
+	// C getCondition();
+
+	// Collection<T> getValues();
+	// Table<V, C, DtNode<C, V, T>> getChildren();
 	
 	default boolean isInnerNode() {
 		return this instanceof InnerNode;
@@ -49,18 +62,27 @@ interface LeafNode<C, V, T>
 interface InnerNode<C, V, T>
 	extends DtNode<C, V, T>
 {
-	C getCondition();
+	C getClassifier();
 
-	InnerNode<C, V, T> setCondition(C condition);
-	
-	/** Create a new inner node for that value. Replaces any existing one for that value */
-	InnerNode<C, V, T> newInnerNode(V value);
+	/** Get the inner node for the given value and classifier; create it if it does not exist */
+	InnerNode<C, V, T> getOrCreateInnerNode(V value, C classifier);
 
-	/** Create a new leaf node for that value. Replaces any existing one for that value */
-	LeafNode<C, V, T> newLeafNode(V value);
-	
+	/** Get the inner node for the given value and classifier; returns null if it does not exist */
+	InnerNode<C, V, T> getInnerNode(V value, C classifier);
+
+	default InnerNode<C, V, T> getOrCreateInnerNode(C classifier) {
+		return getOrCreateInnerNode(null, classifier);
+	}
+
 	/** Lookup a node for a given value */
-	DtNode<C, V, T> getNode(Object value);
+	Collection<? extends InnerNode<C, V, T>> getInnerNodes(Object value);
+
+	/** Get the leaf node for the given value; create it if it does not exist */
+	LeafNode<C, V, T> getOrCreateLeafNode(V value);
+
+	/** Get the leaf node for the given value; returns null if it does not exist */
+	LeafNode<C, V, T> getLeafNode(V value);
+
 }
 
 class DtNodeBase<C, V, T>
@@ -109,6 +131,11 @@ class LeafNodeImpl<C, V, T>
 		this.value = value;
 		return this;
 	}
+	
+	@Override
+	public String toString() {
+		return "Leaf(" + value + ")";
+	}
 }
 
 class InnerNodeImpl<C, V, T>
@@ -117,14 +144,22 @@ class InnerNodeImpl<C, V, T>
 {
 	private static final long serialVersionUID = 7046323103753590235L;
 
-	public InnerNodeImpl(InnerNode<C, V, T> parent, V reachingValue) {
+	public InnerNodeImpl(InnerNode<C, V, T> parent, V reachingValue, C classifier) {
 		super(parent, reachingValue);
-		this.condition = null;
-		this.valueToChild = new LinkedHashMap<>();
+		this.classifier = classifier;
+		
+		// Defer init of maps to first use?
+		this.valueToConditionToInnerNode = HashBasedTable.create(); //new LinkedHashMap<>();
+		this.valueToLeafNode = new HashMap<>();
 	}
 
-	protected C condition;
-	protected Map<V, DtNode<C, V, T>> valueToChild;
+	protected C classifier;
+	// protected Multimap<V, DtNode<C, V, T>> valueToChild;
+	
+	/** Beware of the {@link Optional} vs {@link java.util.Optional} because the latter is NOT serializable...
+	 * and Table does not allow null values */
+	protected Table<Optional<V>, Optional<C>, InnerNode<C, V, T>> valueToConditionToInnerNode;
+	protected Map<V, LeafNode<C, V, T>> valueToLeafNode;
 	
 	@Override
 	public InnerNode<C, V, T> getParent() {
@@ -132,41 +167,82 @@ class InnerNodeImpl<C, V, T>
 	}
 
 	@Override
-	public C getCondition() {
-		return condition;
+	public C getClassifier() {
+		return classifier;
 	}
 
+//	@Override
+//	public InnerNode<C, V, T> setCondition(C condition) {
+//		this.condition = condition;
+//		return this;
+//	}
+	
 	@Override
-	public InnerNode<C, V, T> setCondition(C condition) {
-		this.condition = condition;
-		return this;
+	public Collection<? extends InnerNode<C, V, T>> getInnerNodes(Object value) {
+		Optional<?> rowKey = Optional.fromNullable(value);
+
+		// Ensure that rowKey is in 'valueToConditionToInnerNode' as to prevent the
+		// cast '(V)value' from raising an exception
+		@SuppressWarnings("unchecked")
+		Collection<? extends InnerNode<C, V, T>> result = valueToConditionToInnerNode.rowKeySet().contains(rowKey)
+			? valueToConditionToInnerNode.row(Optional.fromNullable((V)value)).values()
+			: Collections.emptySet();
+		
+		return result; 
+	}
+
+
+	@Override
+	public InnerNode<C, V, T> getInnerNode(V value, C classifier) {
+		InnerNode<C, V, T> result = valueToConditionToInnerNode.get(Optional.fromNullable(value), Optional.fromNullable(classifier));
+		return result;
 	}
 	
 	@Override
-	public DtNode<C, V, T> getNode(Object value) {
-		return valueToChild.get(value);
-	}
+	public InnerNode<C, V, T> getOrCreateInnerNode(V value, C classifier) {
+		InnerNode<C, V, T> result = getInnerNode(value, classifier);
 
-	@Override
-	public InnerNode<C, V, T> newInnerNode(V value) {
-		InnerNode<C, V, T> result = new InnerNodeImpl<>(this, value);
-		valueToChild.put(value, result);
+		if (result == null) {
+			result = new InnerNodeImpl<>(this, value, classifier);
+			valueToConditionToInnerNode.put(Optional.fromNullable(value), Optional.fromNullable(classifier), result);
+		}
+
 		return result;
 	}
 
 	@Override
-	public LeafNode<C, V, T> newLeafNode(V value) {
-		LeafNode<C, V, T> result = new LeafNodeImpl<>(this, value);
-		valueToChild.put(value, result);
+	public LeafNode<C, V, T> getLeafNode(Object value) {
+		return valueToLeafNode.get(value);
+	}
+
+	@Override
+	public LeafNode<C, V, T> getOrCreateLeafNode(V value) {
+		LeafNode<C, V, T> result = getLeafNode(value);
+		
+		if (result == null) {
+			result = new LeafNodeImpl<>(this, value);
+			//valueToLeafNode.put(Optional.fromNullable(value), result);
+			valueToLeafNode.put(value, result);
+		}
 		return result;
-	}	
+	}
+	
+	@Override
+	public String toString() {
+		return "Inner(" + classifier + ", " + valueToLeafNode + ", " + valueToConditionToInnerNode + ")";
+	}
+
 }
 
+//class SparqlInnerNode<T>
+//
+//{	
+//}
 
 class DecisionTreeSparqlBase<T>
 	implements Serializable
 {
-	protected InnerNode<Expr, Node, T> root = new InnerNodeImpl<>(null, null);
+	protected InnerNode<Expr, Node, T> root = new InnerNodeImpl<>(null, null, null);
 	
 	public InnerNode<Expr, Node, T> getRoot() {
 		return root;
@@ -177,15 +253,26 @@ class DecisionTreeSparqlBase<T>
 			outResults.add(node.asLeafNode());
 		} else {
 			InnerNode<Expr, Node, T> innerNode = node.asInnerNode();
-			Expr cond = innerNode.getCondition();
+			Expr cond = innerNode.getClassifier();
 			
-			// TODO Check whether all mentioned variables are bound
-			NodeValue nv = ExprUtils.eval(cond, binding);
-			Node eval = nv.asNode();
-			
-			DtNode<Expr, Node, T> child = innerNode.getNode(eval);
-			
-			recursiveFind(binding, child, outResults);
+			Node eval;
+			if (cond == null) {
+				eval = null;
+			} else {
+				// FIXME Check whether all mentioned variables are bound?
+				NodeValue nv = ExprUtils.eval(cond, binding);
+				eval = nv.asNode();
+			}			
+
+			LeafNode<Expr, Node, T> leaf = innerNode.getLeafNode(eval);
+			if (leaf != null) {
+				recursiveFind(binding, leaf, outResults);
+			}
+
+			Collection<? extends InnerNode<Expr, Node, T>> inners = innerNode.getInnerNodes(eval);			
+			for (DtNode<Expr, Node, T> child : inners) {
+				recursiveFind(binding, child, outResults);
+			}
 		}
 	}
 	
@@ -221,38 +308,45 @@ class DecisionTreeSparqlExpr
 }
 
 
-/** Decision tree where leafs carry sets of expressions */
-class DecisionTreeSparqlExprSet
-	extends DecisionTreeSparqlBase<Set<Expr>>
-{
-	private static final long serialVersionUID = 5760959713235187882L;
-
-	public Node eval(Binding binding) {
-		Collection<LeafNode<Expr, Node, Set<Expr>>> leafs = findLeafNodes(binding);
-		
-		Node result;
-		if (leafs.isEmpty()) {
-			result = null;
-		} else {
-			LeafNode<Expr, Node, Set<Expr>> leaf = Iterables.getOnlyElement(leafs);
-			Set<Expr> exprs = leaf.getValue();
-			
-//			Set<Node> vals = new HashSet<>();
-			result = null;
-			for (Expr expr : exprs) {
-				NodeValue nv = ExprUtils.eval(expr, binding);
-				Node node = nv == null ? null : nv.asNode();
-				
-				if (node != null) {
-					result = node;
-					break;
-				}
-			}
-		}
-		
-		return result;
-	}
-}
+/**
+ * Decision tree where leafs carry sets of expressions
+ * This is de-facto a convenience representation where expressions in the leaf nodes
+ * implicitly have BOUND(?var) conditions for each variable mentioned in the expressions.
+ * 
+ * For this reason, this class may become deprecated.
+ * 
+ */
+//class DecisionTreeSparqlExprSet
+//	extends DecisionTreeSparqlBase<Set<Expr>>
+//{
+//	private static final long serialVersionUID = 5760959713235187882L;
+//
+//	public Node eval(Binding binding) {
+//		Collection<LeafNode<Expr, Node, Set<Expr>>> leafs = findLeafNodes(binding);
+//		
+//		Node result;
+//		if (leafs.isEmpty()) {
+//			result = null;
+//		} else {
+//			LeafNode<Expr, Node, Set<Expr>> leaf = Iterables.getOnlyElement(leafs);
+//			Set<Expr> exprs = leaf.getValue();
+//			
+////			Set<Node> vals = new HashSet<>();
+//			result = null;
+//			for (Expr expr : exprs) {
+//				NodeValue nv = ExprUtils.eval(expr, binding);
+//				Node node = nv == null ? null : nv.asNode();
+//				
+//				if (node != null) {
+//					result = node;
+//					break;
+//				}
+//			}
+//		}
+//		
+//		return result;
+//	}
+//}
 
 
 
@@ -269,11 +363,13 @@ public class DecisionTree {// <I, C, T, N extends DtNode<C, T>> {
 	public static void main(String[] args) {
 		DecisionTreeSparqlExpr dt = new DecisionTreeSparqlExpr();
 		
-		dt.getRoot().setCondition(ExprUtils.parse("BOUND(?p)"));
-		LeafNode<Expr, Node, Expr> yes = dt.getRoot().newLeafNode(NodeValue.TRUE.asNode());
+		InnerNode<Expr, Node, Expr> dn = dt.getRoot().getOrCreateInnerNode(ExprUtils.parse("BOUND(?p)"));
+		
+		
+		LeafNode<Expr, Node, Expr> yes = dn.getOrCreateLeafNode(NodeValue.TRUE.asNode());
 		yes.setValue(ExprUtils.parse("'yay'"));
 
-		LeafNode<Expr, Node, Expr> no = dt.getRoot().newLeafNode(NodeValue.FALSE.asNode());
+		LeafNode<Expr, Node, Expr> no = dn.getOrCreateLeafNode(NodeValue.FALSE.asNode());
 		no.setValue(ExprUtils.parse("'nah'"));
 
 		Node result1 = dt.eval(BindingFactory.binding(Vars.p, NodeFactory.createLiteral("test")));
@@ -284,4 +380,33 @@ public class DecisionTree {// <I, C, T, N extends DtNode<C, T>> {
 
 	}
 	// Stream<> find(I input);
+}
+
+
+/**
+ * Var definitions based on decision tree structures.
+ * This allows for the use of 'discriminator' columns, such is
+ * ?x != (if (?p = 1) then IRI(STR(?o)) if (?p = 0) then ?o) 
+ * 
+ * 
+ * @author raven
+ *
+ */
+class ConditionalVarDefinition {
+	protected Map<Var, DecisionTreeSparqlExpr> definitions;
+	
+	protected ConditionalVarDefinition() {
+		this(new LinkedHashMap<>());
+	}
+
+	public ConditionalVarDefinition(Map<Var, DecisionTreeSparqlExpr> definitions) {
+		super();
+		this.definitions = definitions;
+	}
+	
+	public ConditionalVarDefinition put(Var var, DecisionTreeSparqlExpr definition) {
+		definitions.put(var, definition);
+		return this;
+	}
+	
 }
