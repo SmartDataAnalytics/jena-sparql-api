4608d9c4cc1dc21fb4ded6236250faacfad99415
Claus Stadler <RavenArkadon@googlemail.com>
2021-06-20 02:47:43 +0200

    Minor clean up of a util class

--- jena-sparql-api-collections/src/main/java/org/aksw/jena_sparql_api/utils/views/map/MapFromKeyConverter.java
+++ jena-sparql-api-collections/src/main/java/org/aksw/jena_sparql_api/utils/views/map/MapFromKeyConverter.java
@@ -4,70 +4,11 @@ import java.util.AbstractMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
-import java.util.function.Function;
 
 import org.aksw.commons.collections.ConvertingCollection;
 import org.aksw.commons.collections.sets.SetFromCollection;
 
 import com.google.common.base.Converter;
-
-class EntryConverterByKey<K, L, V>
-    extends Converter<Entry<K, V>, Entry<L, V>>
-{
-    protected Converter<K, L> converter;
-
-    public EntryConverterByKey(Converter<K, L> converter) {
-        super();
-        this.converter = converter;
-    }
-
-    @Override
-    protected Entry<L, V> doForward(Entry<K, V> a) {
-        return transformKey(a, converter::convert);
-    }
-
-    @Override
-    protected Entry<K, V> doBackward(Entry<L, V> b) {
-        return transformKey(b, converter.reverse()::convert);
-    }
-
-    public static <K, L, V> Converter<Entry<K, V>, Entry<L, V>> converterByKey(Converter<K, L> converter) {
-        return Converter.from(a -> transformKey(a, converter::convert), b -> transformKey(b, converter.reverse()::convert));
-    }
-
-    public static <K, V, W> Converter<Entry<K, V>, Entry<K, W>> converterByValue(Converter<V, W> converter) {
-        return Converter.from(a -> transformValue(a, converter), b -> transformValue(b, converter.reverse()));
-    }
-
-
-
-    public static <K, L, V> Entry<L, V> transformKey(Entry<K, V> e, Function<? super K, ? extends L> fn) {
-        L key = fn.apply(e.getKey());
-        return new Entry<L, V>() {
-            @Override public L getKey() { return key; }
-            @Override public V getValue() { return e.getValue(); }
-            @Override public V setValue(V v) { return e.setValue(v); }
-        };
-    }
-
-    public static <K, V, W> Entry<K, W> transformValue(Entry<K, V> e, Converter<V, W> converter) {
-        K key = e.getKey();
-        return new Entry<K, W>() {
-            @Override public K getKey() { return key; }
-            @Override public W getValue() {
-                V raw = e.getValue();
-                W r = converter.convert(raw);
-                return r;
-            }
-            @Override public W setValue(W v) {
-                V raw = converter.reverse().convert(v);
-                e.setValue(raw);
-                return v;
-            }
-        };
-    }
-
-}
 
 public class MapFromKeyConverter<K, J, V>
     extends AbstractMap<K, V>
